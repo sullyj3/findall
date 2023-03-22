791bd50b7c7f8bf1df8722199bd356dc00cd585b
James Sully <sullyj3@gmail.com>
2023-03-22 14:21:46 +1100

    treat option second argument as target

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,26 @@ use std::fs::File;
 use std::io::Read;
 use walkdir::WalkDir;
 
+fn usage() -> ! {
+    eprintln!("Usage: {} <space separated patterns> <path>", env::args().next().unwrap());
+    std::process::exit(1);
+}
+
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().skip(1).collect();
+    let path = match args.len() {
+        0 => usage(),
+        1 => ".".to_string(),
+        2 => args[1].to_string(),
+        _ => usage(),
+    };
 
-    if args.is_empty() {
-        eprintln!("Usage: <program_name> <string1> <string2> ...");
-        std::process::exit(1);
-    }
+    // we have one or two arguments
+    // interpret the first argument as a space separated list of patterns,
+    let patterns: Vec<String> = args[0].split(' ').map(|s| s.to_string()).collect();
+    let ac = AhoCorasick::new(&patterns);
 
-    let ac = AhoCorasick::new(&args);
-
-    for entry in WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
+    for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
         if entry.file_type().is_file() {
             let path = entry.path();
             let mut file = File::open(&path)
