32cfc7ab0a3255754d8d46f8ffbfef901e072953
James Sully <sullyj3@gmail.com>
2023-06-05 13:43:45 +1000

    Thiings

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use aho_corasick::AhoCorasick;
 use anyhow::{Context, Result};
 use std::env;
 use std::fs::File;
+use std::path::Path;
 use std::io::Read;
 use walkdir::WalkDir;
 use std::collections::HashMap;
@@ -12,18 +13,81 @@ fn usage() -> ! {
     std::process::exit(1);
 }
 
-fn main() -> Result<()> {
-    let args: Vec<String> = env::args().skip(1).collect();
-    let path = match args.len() {
-        0 => usage(),
-        1 => ".".to_string(),
-        2 => args[1].to_string(),
-        _ => usage(),
-    };
+enum Mode {
+    Default,
+    FilenameOnly,
+}
 
-    // we have one or two arguments
-    // interpret the first argument as a space separated list of patterns,
-    let patterns: Vec<String> = args[0].split(' ').map(|s| s.to_string()).collect();
+struct Invocation {
+    mode: Mode,
+    patterns: Vec<String>,
+    path: String,
+}
+
+impl Default for Invocation {
+    fn default() -> Self {
+        let default_path: String = ".".to_string();
+        Invocation {
+            mode: Mode::Default,
+            patterns: Vec::new(),
+            path: default_path,
+        }
+    }
+}
+
+impl Invocation {
+    fn with_mode(mut self, mode: Mode) -> Self {
+        self.mode = mode;
+        self
+    }
+
+    fn with_patterns(mut self, patterns: Vec<String>) -> Self {
+        self.patterns = patterns;
+        self
+    }
+
+    fn with_path(mut self, path: String) -> Self {
+        self.path = path;
+        self
+    }
+}
+
+fn split_patterns(patterns: &str) -> Vec<String> {
+    patterns.split(' ').map(|s| s.to_string()).collect()
+}
+
+fn parse_args() -> Option<Invocation> {
+    let args: Vec<String> = env::args().skip(1).collect();
+    let arg_strs: Vec<&str> = args.iter().map(|s| s.as_str()).collect();
+
+    match arg_strs[..] {
+        [] => None,
+        ["-l", patterns] => Some(
+            Invocation::default()
+                .with_mode(Mode::FilenameOnly)
+                .with_patterns(split_patterns(patterns))
+            ),
+        ["-l", patterns, path] => Some(
+            Invocation::default()
+                .with_mode(Mode::FilenameOnly)
+                .with_patterns(split_patterns(patterns))
+                .with_path(path.to_string())
+            ),
+        [patterns] => Some(
+            Invocation::default()
+                .with_patterns(split_patterns(patterns))
+            ),
+        [patterns, path] => Some(
+            Invocation::default()
+                .with_patterns(split_patterns(patterns))
+                .with_path(path.to_string())
+            ),
+        _ => None,
+    }
+}
+
+fn main() -> Result<()> {
+    let Invocation {mode, patterns, path} = parse_args().unwrap_or_else(|| usage());
     let ac = AhoCorasick::new(&patterns);
 
     for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
@@ -40,14 +104,17 @@ fn main() -> Result<()> {
 
             let counts = match_counts(&ac, &patterns, &contents);
             if counts.values().all(|&x| x > 0) {
-                let stripped_path = path.strip_prefix("./").unwrap_or(path);
+                let stripped_path: &Path = path.strip_prefix("./").unwrap_or(path);
                 println!("{}", stripped_path.display().to_string().red());
-                let match_counts_string = patterns.iter().map(|pattern| {
-                    let count = counts.get(pattern).unwrap();
-                    format!("{}: {}", pattern.blue(), count)
-                }).collect::<Vec<String>>()
-                  .join(", ");
-                println!("    {}", match_counts_string);
+
+                if let Mode::Default = mode {
+                    let match_counts_string = patterns.iter().map(|pattern| {
+                        let count = counts.get(pattern).unwrap();
+                        format!("{}: {}", pattern.blue(), count)
+                    }).collect::<Vec<String>>()
+                      .join(", ");
+                    println!("    {}", match_counts_string);
+                }
             }
         }
     }
